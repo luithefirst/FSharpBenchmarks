89aaa7dacb8098d7f6917f295346a26864a70878
Christian Luksch <luksch@vrvis.at>
2023-05-10 23:54:08 +0200

    faster LinkedListRef implementation

--- FSharpBenchmarks/LinkedListBench.fs
+++ FSharpBenchmarks/LinkedListBench.fs
@@ -1,4 +1,29 @@
-﻿
+﻿// This benchmark evaluates the performance difference between Option, ValueOption, and Reference/Pointer
+//
+// This blog claims that the compiler might optimize options of reference types to references directly:
+// https://www.bartoszsypytkowski.com/writing-high-performance-f-code/
+//  -> for ValueOptions this would sound reasonable
+// 
+// Conclusion:
+//  - ValueOption<ReferenceType> still stores a "HasValue" separately
+//  - Matching and updating ValueOptions does not run as fast as "pure" references
+//  - The implementation using references (pointers) with [<AllowNullLiteral>] is somehow slower than ValueOptions -> could not run the DisassemblyDiagnoser to investigate further
+
+
+//BenchmarkDotNet=v0.13.5, OS=Windows 10 (10.0.19044.2846/21H2/November2021Update)
+//Intel Core i7-8700K CPU 3.70GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+//.NET SDK=7.0.203
+//  [Host]     : .NET 7.0.5 (7.0.523.17405), X64 RyuJIT AVX2 DEBUG
+//  DefaultJob : .NET 7.0.5 (7.0.523.17405), X64 RyuJIT AVX2
+
+
+//|     Method | Count |     Mean |     Error |    StdDev | Ratio | RatioSD | Code Size |   Gen0 |   Gen1 |   Gen2 | Allocated | Alloc Ratio |
+//|----------- |------ |---------:|----------:|----------:|------:|--------:|----------:|-------:|-------:|-------:|----------:|------------:|
+//|    OptList |   100 | 9.261 us | 0.4956 us | 1.4612 us |  1.00 |    0.00 |   1,721 B | 0.8850 | 0.8240 | 0.1526 |   5.47 KB |        1.00 |
+//| ValOptList |   100 | 6.239 us | 0.1234 us | 0.3377 us |  0.69 |    0.11 |   1,694 B | 0.4425 | 0.1755 | 0.0076 |   2.73 KB |        0.50 |
+//|    RefList |   100 | 5.378 us | 0.1075 us | 0.2596 us |  0.59 |    0.09 |   1,603 B | 0.3128 | 0.1068 | 0.0076 |   1.95 KB |        0.36 |
+
+
 module LinkedListBench
     
     open BenchmarkDotNet.Attributes
@@ -106,7 +131,7 @@ module LinkedListBench
             | ValueNone -> tail <- item.Prev
 
 
-    [<AllowNullLiteral>] 
+    // NOTE: using [<AllowNullLiteral>] is somehow much slower
     type ListItemRef<'a>(value : 'a) =
         
         let mutable next : ListItemRef<'a> = Unchecked.defaultof<_>
@@ -140,10 +165,10 @@ module LinkedListBench
             item.Prev <- before.Prev
 
             let p = before.Prev
-            if p <> null then
-                p.Next <- item
-            else
+            if Object.ReferenceEquals(p, null) then
                 head <- item
+            else
+                p.Next <- item
 
             before.Prev <- item
 
@@ -152,21 +177,22 @@ module LinkedListBench
         member x.Remove(item : ListItemRef<'a>) =
             
             let p = item.Prev
-            if p <> null then
-                p.Next <- item.Next
-            else
+            if Object.ReferenceEquals(p, null) then
                 head <- item.Next
+            else
+                p.Next <- item.Next
 
             let n = item.Next
-            if n <> null then
-                n.Prev <- item.Prev
-            else
+            if Object.ReferenceEquals(n, null) then
                 tail <- item.Prev
+            else
+                n.Prev <- item.Prev
 
 
-    [<InProcess>]
+    //[<InProcess>] // results in crash with DisassemblyDiagnoser
     [<PlainExporter; MemoryDiagnoser>]
-    //[<DisassemblyDiagnoser(5, BenchmarkDotNet.Diagnosers.DisassemblySyntax.Intel, false, false, false, false, false, false)>]
+    //[<DisassemblyDiagnoser(5, BenchmarkDotNet.Diagnosers.DisassemblySyntax.Masm, true, true, true, true, true, true)>]
+    //[<RyuJitX64Job>]
     type LinkedListBench() =
         let mutable optList = LinkedListOpt<_>(5)
         let mutable optListItems = System.Collections.Generic.List<ListItemOpt<_>>(10000)
@@ -243,7 +269,7 @@ module LinkedListBench
                 x.RefListAdd()
 
 
-        [<Benchmark(Baseline = true)>]
+        [<Benchmark>]
         member x.OptList() =
             for i in 1..x.Count do
                 if optListItems.Count < 10000 && (rnd.Next(2) = 0) then
